eda078978c747215523ea0a6683cfb29c8cfc479
Stephen Schleising <stephen@schleising.net>
2020-08-16 13:22:02 +0100

    Rename in urls.py for Apps so there is only one index

--- mainsite/schitter/urls.py
+++ mainsite/schitter/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.index, name='schitter'),
     path('take_a_schitt/', views.take_a_schitt, name='take_a_schitt'),
     path('log_schitt/', views.log_schitt, name='log_schitt'),
 ]
