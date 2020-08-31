edc1c5ac70eee0b667eb69e6ac71696a90985f76
Stephen Schleising <stephen@schleising.net>
2020-08-31 10:10:53 +0100

    Enable quick restart of all containers

---
+++ start-website.sh
@@ -0,0 +1,4 @@
+#!/bin/zsh
+/Users/steve/Documents/Coding/Docker/nginx-image/run_container.sh &
+/Users/steve/Documents/Coding/Docker/uwsgi-image/run_container.sh &
+/Users/steve/Documents/Coding/Docker/bokeh-server-image/run-container.sh &
