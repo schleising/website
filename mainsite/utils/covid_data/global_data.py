824d0accbd64231aa7fcab621291627a50212e99
Stephen Schleising <stephen@schleising.net>
2020-10-24 11:45:41 +0100

    Remove printout and fix spelling

--- mainsite/utils/covid_data/global_data.py
+++ mainsite/utils/covid_data/global_data.py
@@ -6,11 +6,7 @@ from django.core.cache import cache
 json_url = 'https://pomber.github.io/covid19/timeseries.json'
 
 def GetGlobalData():
-    print()
-    print('Got Global Data')
-    print()
-
-    raw_data = cache.get_or_set('golbal-data', requests.get(json_url), 5 * 60)
+    raw_data = cache.get_or_set('global-data', requests.get(json_url), 5 * 60)
 
     json_data = raw_data.json()
 
