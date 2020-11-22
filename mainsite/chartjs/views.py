d36dce6b4eb18cae25f2bb3e4a367b181c8da849
Stephen Schleising <stephen@schleising.net>
2020-11-22 21:08:28 +0000

    Start migration to chartjs, daily cases and daily deaths first

--- mainsite/chartjs/views.py
+++ mainsite/chartjs/views.py
@@ -10,15 +10,14 @@ import pandas as pd
 
 from .libs.chart_js_dataclass import ChartJS, Dataset
 
-data_request_function_dict = {}
-
-# Generate Average Daily Cases Data
-def GenerateAverageDailyCases(country):
+# Generate Average Data
+def GenerateAverages(country, data_request):
     json_data = GetGlobalData()
 
     df = pd.DataFrame(json_data[country])
 
     df['daily_confirmed'] = df['confirmed'].diff()
+    df['daily_deaths']    = df['deaths'].diff()
 
     # Use DataFrame.rolling() to generate running average data
     rolling = df.rolling(7)
@@ -27,15 +26,14 @@ def GenerateAverageDailyCases(country):
     mean = rolling.mean()
 
     # Add the rolling mean columns to the data frame
-    df['MeanDailyConfirmed'] = mean['daily_confirmed'].round()
+    df['AverageDailyCases']  = mean['daily_confirmed'].round()
+    df['AverageDailyDeaths'] = mean['daily_deaths'].round()
 
     df.fillna(0, inplace=True)
 
-    average_daily_cases = df['MeanDailyConfirmed'].to_list()
+    averages = df[data_request].to_list()
 
-    data_request_function_dict['AverageDailyCases'] = GenerateAverageDailyCases
-    
-    return average_daily_cases
+    return averages
 
 # Create your views here.
 def NewCasesAgainstTime(request):
@@ -49,7 +47,7 @@ def NewCasesAgainstTime(request):
 
     date = df['date'].to_list()
 
-    average_daily_cases = GenerateAverageDailyCases(default_country)
+    average_daily_cases = GenerateAverages(default_country, 'AverageDailyCases')
 
     chart = ChartJS()
 
@@ -65,12 +63,40 @@ def NewCasesAgainstTime(request):
 
     return render(request, 'chartjs/global_new_cases.html', context)
 
+# Create your views here.
+def DeathsAgainstTime(request):
+    default_country = 'United Kingdom'
+
+    json_data = GetGlobalData()
+
+    countries = [country for country in json_data]
+
+    df = pd.DataFrame(json_data[default_country])
+
+    date = df['date'].to_list()
+
+    average_daily_cases = GenerateAverages(default_country, 'AverageDailyDeaths')
+
+    chart = ChartJS()
+
+    chart.SetLabel('Daily Deaths')
+    chart.SetCategories(date)
+    chart.SetyAxis('logarithmic')
+
+    chart.AddDataset(default_country, average_daily_cases, 'line', randomColourAsRGBA(default_country))
+
+    context = {}
+    context['chart'] = chart.chartjs_dict
+    context['countries'] = countries
+
+    return render(request, 'chartjs/global_deaths.html', context)
+
 def CheckboxClicked(request : WSGIRequest):
     country = request.GET.get('country')
     data_request = request.GET.get('DataRequest')
 
     # For future reference, this is a function call !!
-    data = data_request_function_dict[data_request](country)
+    data = GenerateAverages(country, data_request)
 
     dataset = Dataset()
     dataset.AddDataset(f'{country}', data, 'line', randomColourAsRGBA(country))
