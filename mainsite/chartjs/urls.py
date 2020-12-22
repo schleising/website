38ba59c20461969e7cbe8b1126fea220d595338b
schleising <stephen@schleising.net>
2020-12-22 13:37:58 +0000

    Update daily confirmed vs total confirmed to use chartjs

--- mainsite/chartjs/urls.py
+++ mainsite/chartjs/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     # path('', views.daily_confirmed_total_confirmed, name='daily_confirmed_total_confirmed'),
     path('global_new_cases', views.NewCasesAgainstTime, name='global_new_cases'),
     path('global_deaths', views.DeathsAgainstTime, name='global_deaths'),
+    path('daily_cases_total_cases', views.DailyCasesAgainstTotalCases, name='daily_cases_total_cases'),
     path('checkbox_clicked', views.CheckboxClicked, name='checkbox_clicked'),
 ]
--- mainsite/chartjs/views.py
+++ mainsite/chartjs/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from django.core.handlers.wsgi import WSGIRequest
 from django.http.response import JsonResponse
-from numpy.lib.function_base import _average_dispatcher
 
 from utils.covid_data.global_data import GetGlobalData
 from utils.colours.colours import randomColourAsRGBA
@@ -91,12 +90,53 @@ def DeathsAgainstTime(request):
 
     return render(request, 'chartjs/global_deaths.html', context)
 
+def GetDailyCasesAgainstTotalCasesData(country):
+    json_data = GetGlobalData()
+
+    df = pd.DataFrame(json_data[country])
+
+    average_daily_cases = GenerateAverages(country, 'AverageDailyCases')
+    total_cases = df['confirmed'].to_list()
+
+    data = [{ 'x': x_points, 'y': y_points} for x_points, y_points in zip(total_cases, average_daily_cases)]
+
+    return data
+
+# Create your views here.
+def DailyCasesAgainstTotalCases(request):
+    default_country = 'United Kingdom'
+
+    json_data = GetGlobalData()
+
+    countries = [country for country in json_data]
+
+    data = GetDailyCasesAgainstTotalCasesData(default_country)
+
+    chart = ChartJS()
+
+    chart.SetLabel('Daily Cases against Total Cases')
+    chart.SetxAxis('logarithmic')
+    chart.SetyAxis('logarithmic')
+
+    chart.AddDataset(default_country, data, 'scatter', randomColourAsRGBA(default_country))
+
+    context = {}
+    context['chart'] = chart.chartjs_dict
+    context['countries'] = countries
+
+    return render(request, 'chartjs/daily_cases_total_cases.html', context)
+
 def CheckboxClicked(request : WSGIRequest):
     country = request.GET.get('country')
     data_request = request.GET.get('DataRequest')
 
-    # For future reference, this is a function call !!
-    data = GenerateAverages(country, data_request)
+    print(country)
+    print(data_request)
+
+    if data_request == 'DailyCasesTotalCases':
+        data = GetDailyCasesAgainstTotalCasesData(country)
+    else:
+        data = GenerateAverages(country, data_request)
 
     dataset = Dataset()
     dataset.AddDataset(f'{country}', data, 'line', randomColourAsRGBA(country))
